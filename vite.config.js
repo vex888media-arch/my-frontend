7fb6d834d0fd15adcf33a0070c9fcde9fc5b51f1
user <hfyhj@userMacBook-Air.local>
2026-08-15 22:35:55 +0900

    fix tailwind build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,1 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+import { defineConfig } from "vite"; import react from "@vitejs/plugin-react"; export default defineConfig({ plugins: [react()], css: { minify: false } });
