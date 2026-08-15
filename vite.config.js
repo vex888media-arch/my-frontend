5c9b61649eb6ea706ce3628dc26b4568da2c5ea9
user <hfyhj@userMacBook-Air.local>
2026-08-15 22:55:12 +0900

    fix: install tailwindcss and cleanup config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
-import { defineConfig } from "vite"
-import vue from "@vitejs/plugin-vue"
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  css: {
+    transformer: 'lightningcss',
+    lightningcss: {
+      targets: {}
+    }
+  },
   build: {
-    cssMinify: "esbuild"
+    cssMinify: 'esbuild'
   }
 })
